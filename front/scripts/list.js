b6b485d034790f74478c44bd4c793e5677a1eeaf
Quentin BEAUPUY <quentin.beaupuy@versusmind.eu>
2022-03-17 20:42:28 +0100

    ajout pagination

--- front/scripts/list.js
+++ front/scripts/list.js
@@ -1,16 +1,25 @@
 const listDocs = () => {
     return {
+        page: 0,
+        first: true,
+        last: true,
         docs: [],
         formMessage: '',
         async getAll() {
             this.formMessage = '';
             return fetchApi(
-                '/all',
+                '/all/page',
                 'GET',
-                '',
+                {
+                    numPage: this.page,
+                    nbElm: 4
+                },
                 async (response) => {
                     this.formMessage = 'Documents chargés';
-                    return response.json();
+                    const res = await response.json();
+                    this.first = res.first;
+                    this.last = res.last;
+                    return res.content;
                 },
                 (e) => {
                     this.formMessage = 'Qqch n\'a pas fonctionné ...';
@@ -31,6 +40,16 @@ const listDocs = () => {
                     this.formMessage = 'Qqch n\'a pas fonctionné ...';
                 }
             );
+        },
+        async precedent() {
+            if (this.first) return;
+            this.page--;
+            this.docs = await this.getAll();
+        },
+        async suivant() {
+            if (this.last) return;
+            this.page++;
+            this.docs = await this.getAll();
         }
     };
 }
