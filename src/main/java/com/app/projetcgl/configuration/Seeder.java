60012d1a3e18a96add1806eefdee984977b59d15
el-kefif1 <mohamedmehdielkefif@gmail.com>
2022-03-18 14:31:35 +0100

    Seeder Table Document

--- src/main/java/com/app/projetcgl/configuration/Seeder.java
+++ src/main/java/com/app/projetcgl/configuration/Seeder.java
@@ -1,8 +1,11 @@
 package com.app.projetcgl.configuration;
 
 import com.app.projetcgl.exception.ResourceNotFoundException;
+import com.app.projetcgl.model.Document;
 import com.app.projetcgl.model.Type;
+import com.app.projetcgl.service.DocumentService;
 import com.app.projetcgl.service.TypeService;
+import com.app.projetcgl.service.imp.DocumentServiceImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -32,6 +35,28 @@ public class Seeder {
                 }
             }
             log.info("Fin du seeding de la table Type");
+        };
+    }
+
+    @Bean
+    public CommandLineRunner seedingDocuments(DocumentService documentService, TypeService typeService){
+        return args -> {
+            log.info("Début du seeding de la table Document");
+            documentService.saveDocument(new Document("Document 1","E:/doc1.txt",typeService.getTypeById(1)));
+            documentService.saveDocument(new Document("Audio 1","E:/audio.mp3",typeService.getTypeById(2)));
+            documentService.saveDocument(new Document("Vidéo 1","E:/vid1.mp4",typeService.getTypeById(3)));
+            documentService.saveDocument(new Document("Binaire 1","E:/bin1.txt",typeService.getTypeById(4)));
+
+            documentService.saveDocument(new Document("Document 2","E:/doc2.txt",typeService.getTypeById(1)));
+            documentService.saveDocument(new Document("Audio 2","E:/audio.mp3",typeService.getTypeById(2)));
+            documentService.saveDocument(new Document("Vidéo 2","E:/vid2.mp4",typeService.getTypeById(3)));
+            documentService.saveDocument(new Document("Binaire 2","E:/bin2.txt",typeService.getTypeById(4)));
+
+            documentService.saveDocument(new Document("Document 3","E:/doc3.txt",typeService.getTypeById(1)));
+            documentService.saveDocument(new Document("Audio 3","E:/audio.mp3",typeService.getTypeById(2)));
+            documentService.saveDocument(new Document("Vidéo 3","E:/vid3.mp4",typeService.getTypeById(3)));
+            documentService.saveDocument(new Document("Binaire 3","E:/bin3.txt",typeService.getTypeById(4)));
+            log.info("Début du seeding de la table Document");
         };
     }
 }
